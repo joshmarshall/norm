ce02f13f0c5633cebd5f9f9b657cb366e9505fb8
Joshua Marshall <catchjosh@gmail.com>
2014-02-27 01:25:50 -0600

    Field expects a dict-like interface on a Model.

--- norm/field.py
+++ norm/field.py
@@ -9,6 +9,15 @@ class Field(object):
         self._deserialize = deserialize
         self._valid_type = valid_type
 
+    def set_default(self, field_name, model):
+        if callable(self._default):
+            value = self._default()
+        else:
+            value = self._default
+        if self._default:
+            model[field_name] = value
+        return value
+
     def __set__(self, obj, value):
         field_name = get_field_name(obj.__class__, self)
         value = self._coerce(value)
@@ -17,16 +26,39 @@ class Field(object):
             raise TypeError(
                 "Invalid type `{}` for field `{}`".format(
                     type(value), field_name))
-        obj.fields[field_name] = self._serialize(value)
+        if value is not None:
+            value = self._serialize(value)
+        obj[field_name] = value
 
     def __get__(self, obj, objtype):
         field_name = get_field_name(objtype, self)
-        value = obj.fields.setdefault(field_name, self._default)
-        return self._deserialize(value)
+        try:
+            value = obj[field_name]
+        except KeyError:
+            value = self.set_default(field_name, obj)
+        if value is not None:
+            value = self._deserialize(value)
+        return value
 
 
 def get_field_name(cls, field):
     for attr in cls.__dict__:
         if cls.__dict__[attr] == field:
             return attr
-    raise Exception("what?")
+    raise AttributeError(
+        "No field found on model {}".format(cls.__name__))
+
+
+def get_all_field_names(cls):
+    fields = []
+    for attr in cls.__dict__:
+        if isinstance(cls.__dict__[attr], Field):
+            fields.append(attr)
+    return fields
+
+
+def populate_defaults(model):
+    model_class = model.__class__
+    for field_name in get_all_field_names(model_class):
+        field = model_class.__dict__[field_name]
+        field.set_default(field_name, model)
--- tests/test_field.py
+++ tests/test_field.py
@@ -2,36 +2,44 @@ import unittest
 import norm.field
 
 
-class Base(object):
-
-    def __init__(self):
-        self.data = {}
-
-    @property
-    def fields(self):
-        return self.data
-
-
 class TestField(unittest.TestCase):
 
     def test_field_descriptor(self):
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field()
 
         m = Model()
         m.field = "foo"
         self.assertEqual("foo", m.field)
-        self.assertEqual({"field": "foo"}, m.data)
+        self.assertEqual({"field": "foo"}, m)
 
     def test_default(self):
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field(default="bar")
 
         m = Model()
         self.assertEqual("bar", m.field)
+        # should automatically set default value
+        self.assertEqual("bar", m["field"])
+
+    def test_default_callable(self):
+        class Model(dict):
+            field = norm.field.Field(default=lambda: "foo")
+
+        m = Model()
+        self.assertEqual("foo", m.field)
+        self.assertEqual("foo", m["field"])
+
+    def test_field_returns_none(self):
+        class Model(dict):
+            field = norm.field.Field()
+
+        m = Model()
+        self.assertEqual(None, m.field)
+        self.assertFalse("field" in m)
 
     def test_coerce(self):
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field(coerce=str)
 
         m = Model()
@@ -39,7 +47,7 @@ class TestField(unittest.TestCase):
         self.assertEqual("5", m.field)
 
     def test_serialize(self):
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field(serialize=lambda x: "bar")
 
         m = Model()
@@ -47,7 +55,7 @@ class TestField(unittest.TestCase):
         self.assertEqual("bar", m.field)
 
     def test_deserialize(self):
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field(deserialize=lambda x: "bar")
 
         m = Model()
@@ -55,16 +63,91 @@ class TestField(unittest.TestCase):
         self.assertEqual("bar", m.field)
 
     def test_typechecking(self):
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field(unicode)
 
         m = Model()
         with self.assertRaises(TypeError):
             m.field = 5
 
-        class Model(Base):
+        class Model(dict):
             field = norm.field.Field(unicode, coerce=unicode)
 
         # should coerce first...
         m = Model()
         m.field = 5
+
+    def test_get_field_name(self):
+        field_instance = norm.field.Field()
+
+        class Foo(object):
+            field = field_instance
+
+        self.assertEqual(
+            "field", norm.field.get_field_name(Foo, field_instance))
+
+    def test_get_field_name_no_matching_field(self):
+        field_instance = norm.field.Field()
+
+        class Foo(object):
+            # doesn't include the field...
+            pass
+
+        with self.assertRaises(AttributeError):
+            norm.field.get_field_name(Foo, field_instance)
+
+    def test_get_all_field_names(self):
+        class Foo(object):
+            field1 = norm.field.Field()
+            field2 = norm.field.Field()
+
+        fields = norm.field.get_all_field_names(Foo)
+        self.assertEqual(2, len(fields))
+        self.assertTrue("field1" in fields)
+        self.assertTrue("field2" in fields)
+
+    def test_populate_defaults(self):
+        class Foo(dict):
+            field = norm.field.Field(default="foo")
+
+        foo = Foo()
+        norm.field.populate_defaults(foo)
+        self.assertEqual("foo", foo.field)
+        self.assertEqual("foo", foo["field"])
+
+    def test_field_set_default_with_default(self):
+        model = {}
+        field = norm.field.Field(default="foo")
+        result = field.set_default("field", model)
+        self.assertEqual({"field": "foo"}, model)
+        self.assertEqual("foo", result)
+
+    def test_field_set_default_without_default(self):
+        model = {}
+        field = norm.field.Field()
+        result = field.set_default("field", field)
+        self.assertEqual({}, model)
+        self.assertEqual(None, result)
+
+    def test_deserialize_none_value(self):
+        class Model(dict):
+            field = norm.field.Field(deserialize=str)
+
+        model = Model()
+        self.assertEqual(None, model.field)
+
+        # but falsy values should still be deserialized...
+        model["field"] = 0
+        self.assertEqual('0', model.field)
+
+    def test_serialize_none_value(self):
+        class Model(dict):
+            field = norm.field.Field(serialize=str)
+
+        model = Model()
+        model.field = None
+        self.assertEqual(None, model["field"])
+
+        # but falsy values should still be serialized...
+        model.field = 0
+        self.assertEqual('0', model["field"])
